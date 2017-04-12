312b88e0f31aa082709a86616f1da6921f38e479
SakuraKaze <809379750@qq.com>
2017-04-12 15:55:51 +0800

    Update Owner.java(Entity)

---
+++ src/main/java/com/account/entity/Owner.java
@@ -0,0 +1,49 @@
+package com.account.entity;
+
+public class Owner {
+    private String name;
+    private String email;
+    private String password;
+    private Boolean gender;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getGender() {
+        return gender;
+    }
+
+    public void setGender(Boolean gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
