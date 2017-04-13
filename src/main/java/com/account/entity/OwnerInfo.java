2738e12de3d4801280edd4e0c2b34072ff014054
SakuraKaze <809379750@qq.com>
2017-04-13 08:51:36 +0800

    Create OwnerInfo.java(Entity)

---
+++ src/main/java/com/account/entity/OwnerInfo.java
@@ -0,0 +1,40 @@
+package com.account.entity;
+
+public class OwnerInfo {
+    private int ownerId;
+    private Boolean gender;
+    private Integer age;
+    private String bio;
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public Boolean getGender() {
+        return gender;
+    }
+
+    public void setGender(Boolean gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
